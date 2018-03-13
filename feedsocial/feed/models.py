ddb9bc1ce01a04e00fac07fb6e9f6635a3758e1b
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-03-13 14:18:18 -0500

    added num_comments field on content and started tracking last updated date. default ordering of content is now by last_updated date.

--- feedsocial/feed/models.py
+++ feedsocial/feed/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
+
 
 class UserContent(models.Model):
     content = models.TextField(max_length=140)
@@ -7,6 +9,14 @@ class UserContent(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete = models.CASCADE,
     )
+    num_comments = models.IntegerField(default=0)
+    last_updated = models.DateTimeField(auto_now_add=True)
+
+    def __repr__(self):
+        return "{}: {}".format(self.content, self.creator.username)
+
+    class Meta:
+        ordering = ['-last_updated', ]
 
 class ContentComment(models.Model):
     message = models.TextField(max_length=140)
@@ -18,3 +28,14 @@ class ContentComment(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete = models.CASCADE,
     )
+    creation_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.content_parent.creator != self.comment_creator:
+            self.content_parent.last_updated = timezone.now()
+        self.content_parent.num_comments += 1
+        self.content_parent.save()
+        return super(ContentComment, self).save(*args, **kwargs)
+
+    def __repr__(self):
+        return "{}: {}".format(self.message, self.comment_creator.username)
