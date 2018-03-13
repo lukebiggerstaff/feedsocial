c33f97df9402a9afdd2ecb7c39928adc72e81ec3
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-03-13 08:44:36 -0500

    styled FeedSocialUserCreationForm to included bootstrap classes. Had to override UserCreationForm init method in order to add classes to the widget attribute method due to what appears to be a bug, link to stack overflow answer included in comments

--- feedsocial/accounts/forms.py
+++ feedsocial/accounts/forms.py
@@ -1,8 +1,26 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from .models import FeedSocialUser
 
 class FeedSocialUserCreationForm(UserCreationForm):
 
+    def __init__(self, *args, **kwargs):
+        # hack per https://stackoverflow.com/questions/21572701/customize-the-way-usercreationform-looks-in-django
+        # setting widgets in class Meta was not working for password fields
+        # override init method to set class
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs['class'] = 'form-control text-muted'
+        self.fields['password2'].widget.attrs['class'] = 'form-control text-muted'
+
     class Meta(UserCreationForm.Meta):
         model = FeedSocialUser
         fields = UserCreationForm.Meta.fields + ('email', )
+        widgets = {
+            'username'  : forms.TextInput(attrs={
+                'class' : 'form-control text-muted',
+            }),
+            'email'     : forms.EmailInput(attrs={
+                'class' : 'form-control text-muted',
+            }),
+        }
+
