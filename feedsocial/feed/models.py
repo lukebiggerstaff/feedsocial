47f288df20ae8afc66bd2e8c48ba8161dce33e1c
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-03-13 08:48:54 -0500

    added a comment model to eventually add to each content

--- feedsocial/feed/models.py
+++ feedsocial/feed/models.py
@@ -2,7 +2,18 @@ from django.db import models
 from django.conf import settings
 
 class UserContent(models.Model):
-    content = models.TextField(max_length=280)
+    content = models.TextField(max_length=140)
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+    )
+
+class ContentComment(models.Model):
+    message = models.TextField(max_length=140)
+    content_parent = models.ForeignKey(
+        'UserContent',
+        on_delete = models.CASCADE,
+    )
     creator = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete = models.CASCADE,
