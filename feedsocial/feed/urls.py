2d1e60a2c3c5ce4d5195d07f41febd6af29abf8f
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-03-13 08:50:20 -0500

    added profile and create views for feed application

--- feedsocial/feed/urls.py
+++ feedsocial/feed/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import FeedView
+from .views import (FeedView, PFeedView,
+                    ContentCreateView)
 
 urlpatterns = [
     path('', FeedView.as_view(), name='feed'),
+    path('create', ContentCreateView.as_view(), name='create' ),
+    path('profile/<slug:user>', PFeedView.as_view(), name='pfeed'),
 ]
