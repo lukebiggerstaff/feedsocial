1472142186ee2f5c79fd115829f1bc5912cc9eff
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-03-13 11:53:36 -0500

    created url to route creating of comments

--- feedsocial/feed/urls.py
+++ feedsocial/feed/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import (FeedView, PFeedView,
-                    ContentCreateView)
+                    ContentCreateView, CommentCreateView)
 
 urlpatterns = [
     path('', FeedView.as_view(), name='feed'),
     path('create', ContentCreateView.as_view(), name='create' ),
+    path('content/<int:pk>', CommentCreateView.as_view(), name='comment-create'),
     path('profile/<slug:user>', PFeedView.as_view(), name='pfeed'),
 ]
