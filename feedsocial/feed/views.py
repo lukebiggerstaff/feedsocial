ce31ab2916870108e6f0bf5409800ca19b62c8a7
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-03-13 08:51:12 -0500

    added search feature to feedview anda  personal feed for only content created by certain users, also added a create view for content

--- feedsocial/feed/views.py
+++ feedsocial/feed/views.py
@@ -1,21 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, CreateView
+from django.views.generic.edit import FormMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.utils.datastructures import MultiValueDictKeyError
+from accounts.models import FeedSocialUser
 from .models import UserContent
 from .forms import UserContentForm
 
-class FeedView(CreateView):
-    template_name = 'feed/home.html'
+class FeedView(ListView):
+    template_name = 'feed.html'
+    model = UserContent
+
+    def get_queryset(self):
+        try:
+            query = self.request.GET['query']
+        except MultiValueDictKeyError:
+            qs = UserContent.objects.all()
+            return qs
+        else:
+            if query:
+                qs = UserContent.objects.filter(content__icontains=query)
+                return qs
+            qs = UserContent.objects.all()
+            return qs
+
+class PFeedView(ListView):
+    template_name = 'feed.html'
+
+    def get_queryset(self):
+        user = get_object_or_404(FeedSocialUser, username=self.kwargs['user'])
+        qs = UserContent.objects.filter(creator=user)
+        return qs
+
+class ContentCreateView(CreateView):
+    model = UserContent
+    template_name = 'create.html'
     form_class = UserContentForm
     success_url = reverse_lazy('feed')
 
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
-
-    def get_context_data(self, **kwargs):
-        qs = UserContent.objects.all()
-        kwargs['object_list'] = qs
-        return super().get_context_data(**kwargs)
