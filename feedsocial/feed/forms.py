059ef86f21fe84de1a843469b23d1c22dddb0d4b
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-03-13 08:47:54 -0500

    added bootstrap classes to UserContentForm

--- feedsocial/feed/forms.py
+++ feedsocial/feed/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
+from django import forms
 from .models import UserContent
 
 class UserContentForm(ModelForm):
     class Meta:
         model = UserContent
         fields = ['content',]
+        widgets = {
+            'content' : forms.Textarea(attrs={
+                'class' : 'form-control text-muted',
+            })
+        }
