4b07584c5a494494da15c5b275c2ab90272fcf4c
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-03-14 10:45:50 -0500

    updated accounts/urls to use the new modified FeedSocialAuthForm

--- feedsocial/accounts/urls.py
+++ feedsocial/accounts/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from .views import SignupView
+from .forms import FeedSocialAuthForm
 
 urlpatterns = [
     path('signup', SignupView.as_view(), name='signup'),
-    path('login/', auth_views.login, name='login'),
+    path('login/', auth_views.login,{'authentication_form' : FeedSocialAuthForm}, name='login'),
     path('logout/', auth_views.logout, {'next_page' : '/' }, name='logout'),
 ]
 
